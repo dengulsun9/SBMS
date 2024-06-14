9e9472299cf7915e654e5daa22bbd51eafd7abcb
dengulsun9 <dengulsun9@gmail.com>
2024-06-14 16:02:59 +0530

    H2 Demo

---
+++ 15-Web-MVC-H2-DB_App/src/main/java/in/dengulsun9/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package in.dengulsun9.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.dengulsun9.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+}
+ 
